a3b0d0fb0f85a544d568512bf595e05bea468e97
luca-gif <luca.89m@gmail.com>
2022-05-27 17:46:58 +0200

    Aggiunto componente Welcome

--- src/components/WelcomeComponent.vue
+++ src/components/WelcomeComponent.vue
@@ -2,7 +2,47 @@
   <div>
     <div class="container">
 
-      ciao
+       <section class="text-center my-5">
+        
+        
+          <h2>Welcome to Avada Health</h2>
+          <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium   <br> doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo</p>
+        
+
+        <div class="row my-5">
+          
+          <div class="col-3 text-center">
+            <img src="../assets/img/icon-1.png" alt="">
+            <p>SAME DAY APPOINTMENTS</p>
+            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptam accusantim</p>
+          </div>
+
+          <div class="col-3 text-center">
+            <img src="../assets/img/icon-2.png" alt="">
+            <p>WORLD CLASS FACILITIES</p>
+            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptam accusantim</p>
+          </div>
+
+          <div class="col-3 text-center">
+            <img src="../assets/img/icon-3.png" alt="">
+            <p>EXPERT DOCTORS</p>
+            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptam accusantim</p>
+          </div>
+
+          <div class="col-3 text-center">
+            <img src="../assets/img/icon-4.png" alt="">
+            <p>COMPLEMENTARY THERAPIES</p>
+            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptam accusantim</p>
+          </div>
+          
+        </div>
+
+        <div class="staff">
+          
+        </div>
+
+       </section>
+
     </div>
 
   </div>

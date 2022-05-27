ecbceefb362ddbd67f51854affefed0a5a1cc589
luca-gif <luca.89m@gmail.com>
2022-05-27 10:53:47 +0200

    primo commit

---
+++ src/components/FooterComp.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    f
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FooterComp',
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    h
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeaderComp',
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
